a8e2be1c9dbfba9ea3752535f03e84b493cf155b
lance <lance@localhost>
2024-03-10 20:42:01 -0700

    test

--- Lab02/Hello.java
+++ Lab02/Hello.java
@@ -10,7 +10,7 @@ public class Hello {
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("SacStateID - This lie was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
